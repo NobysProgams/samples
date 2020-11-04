1221e1346ee1d9086b30f16b220bcc171d17c2a4
NobysProgams <73927990+NobysProgams@users.noreply.github.com>
2020-11-04 15:27:36 +0900

    Create MISRA2012R2_2-a.c

---
+++ EvelCodeAnalysis/MISRA2012R2_2-a.c
@@ -0,0 +1,17 @@
+typedef struct ST_S2RECT_SAMP_ {
+  short s_lft;
+  short s_top;
+  short s_rgt;
+  short s_btm;
+} ST_S2RECT_SAMP;
+
+int temp_func_c( void ) {
+  int result = 0;
+  int result2;
+
+  ST_S2RECT_SAMP st_samp = {0};
+  ST_S2RECT_SAMP st_samp2 = {1};
+  st_samp = st_samp2;
+  
+  return result;
+}
